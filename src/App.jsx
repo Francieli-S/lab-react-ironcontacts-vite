9bcd70b2f7c95097308ed7db4b4c256a4f3744cd
Francieli Sauthier <francieli.sauthier@gmail.com>
2023-05-09 21:56:32 +0200

    sort and delete not working

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,73 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
+import contactsData from './contacts.json'
+
+const fiveContacts = contactsData.slice(0, 5)
+const restContacts = contactsData.slice(5, contactsData.length)
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [contacts, setContacts] = useState(fiveContacts)
+
+  const addContact = () => {
+    let randomContact = restContacts[Math.floor(Math.random() * restContacts.length)]
+    setContacts((prevContacts) => [...prevContacts, randomContact])
+  }
+
+  const sortByPop = () => {
+    setContacts((prevContacts) => {
+      prevContacts.sort((a, b) => {
+        return a.popularity.toFixed(2) > b.popularity.toFixed(2) ? 1 : -1
+      })
+    })
+  }
+
+  const sortByName = () => {
+    setContacts((prevContacts) => {
+      prevContacts.sort((a, b) => {
+        return a.name > b.name ? 1 : -1
+      })
+    })
+  }
+
+  const deleteContact = (contactClicked) => {
+    const filterList = contacts.filter((contact) => {
+      return contact !== contactClicked
+    })
+    setContacts(filterList)
+  }
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>IronContacts</h1>
+      <button onClick={addContact}>Add radom contact</button>
+      <button onClick={sortByPop}>Sort by popularity</button>
+      <button onClick={sortByName}>Sort by name</button>
+      <table>
+        <thead>
+          <tr>
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Popularity</th>
+            <th>Won Oscar</th>
+            <th>Won Emmy</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {contacts.map((contact) => (
+            <tr key={contact.id}>
+              <td>
+                <img className="contactPhoto" src={contact.pictureUrl} alt="Contact face" />
+              </td>
+              <td>{contact.name}</td>
+              <td>{contact.popularity.toFixed(2)}</td>
+              <td>{contact.wonOscar && 'trophy'}</td>
+              <td>{contact.wonEmmy && 'trophy'}</td>
+              <button onClick={deleteContact}>Delete</button>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   )
 }
